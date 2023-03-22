df461d063724dc4dc7f40c9c5fbe1e3daee29010
Ozy-Viking <zthankin@gmail.com>
2023-03-22 20:40:13 +1100

    Intial commit

---
+++ onedrive_sync/__init__.py
@@ -0,0 +1,8 @@
+#!/bin/python3.11
+
+def main():
+    ...
+
+
+if __name__ == '__main__':
+    main()
---
+++ onedrive_sync/__main__.py
@@ -0,0 +1,12 @@
+#!/bin/python3.11
+import click
+import loguru
+from icecream import ic
+
+
+def main():
+    ...
+
+
+if __name__ == '__main__':
+    main()
